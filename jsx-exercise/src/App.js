a425a9ae693cddf84fa4795aeaaf8d4a31e36ce1
alzeirjaf <irajakbar03@gmail.com>
2025-03-07 21:47:25 -0700

    exercise 1

---
+++ jsx-exercise/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+  const isLoggedIn = true; 
+
+  return (
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>{currentYear}</p>
+      <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ jsx-exercise/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Home;
+
+
